4e1264fe8cd58c23aa48d0832acc7d70f1e227e7
Robert-John van Doesburg <rjvandoesburg@gmail.com>
2012-11-14 14:17:38 +0100

    Default template

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
+<?php 
+session_start();
+ob_start();
 
-<form action="login.php" method="post">
-    <input type="text" name="login" value="test"/><br/>
-    <input type="password" name="password" value="test"/><br/>
-    <input type="submit" name="" value="Sturen"/><br/>
-</form>
+$page = isset($_GET['page']) ? $_GET['page']:'home';
+
+if(!file_exists(DOCROOT . '/templates/' . $_GET['page'] . '.php'))
+{
+	$page = 'home';
+}
+?>
+<html>
+	<head>
+		<title>Juridische hulp</title>
+		<link rel="stylesheet" type="text/css" href="/styles/default.css" />
+		
+		<script type="text/javascript" src="/scripts/jquery.1.8.3.js"></script>
+		<script type="text/javascript" src="/scripts/default.js"></script>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<div id="header-image"></div>
+				<div id="menu"></div>
+			</div>
+			<div id="content">
+				<div id="left-content">
+					<?php include '/templates/' . $page . '.php';?>
+				</div>
+				<div id="right-content"></div>
+			</div>
+			<div id="footer"></div>
+		</div>
+	</body>
+</html>
+<?php 
+ob_end_flush();
+?>
--- login.php
+++ login.php
@@ -1,21 +1,31 @@
 <?php
+include_once('/inc/config.inc.php');
+include_once(DOCROOT . 'inc/mysql_connect.php'); // bevat functie om verbinding te maken met een database
 
-include_once('/inc/mysql_connect.php'); // bevat functie om verbinding te maken met een database
-
-if(!isset($_POST['login'])) {               // Controleren of beide velden (naam + wachtwoord zijn ingevuld)
-    print("Geen gebruikersnaam ingevuld!");
-} else if(!isset($_POST['password'])) {
-    print("Geen wachtwoord ingevuld!");
+if(isset($_POST))
+{
+	if(!isset($_POST['login'])) {               // Controleren of beide velden (naam + wachtwoord zijn ingevuld)
+	    print("Geen gebruikersnaam ingevuld!");
+	} else if(!isset($_POST['password'])) {
+	    print("Geen wachtwoord ingevuld!");
+	}
+else {
+	$db = connectToDatabase(); // Database verbinding wordt aangemaakt en vastgelegd in $db
+	
+	$user = $db->escape_string($_POST['login']);   // De gebruikersnaam wordt tegen SQL-injectie beveiligd
+	$pass = $db->escape_string($_POST['password']);// Het wachtwoord wordt tegen SQL-injectie beveiligd
+	
+	$res = $db->query("SELECT * FROM users WHERE username='".$user."' AND password='".$pass."' "); // Kijkt in de database of er een gebruiker bestaat met het opgegeven naam + wachtwoord
+	
+	print($db->error);
+	
+	print_r($res);
+	
 }
-
-$db = connectToDatabase(); // Database verbinding wordt aangemaakt en vastgelegd in $db
-        
-$user = $db->escape_string($_POST['login']);   // De gebruikersnaam wordt tegen SQL-injectie beveiligd
-$pass = $db->escape_string($_POST['password']);// Het wachtwoord wordt tegen SQL-injectie beveiligd
-
-$res = $db->query("SELECT * FROM users WHERE username='".$user."' AND password='".$pass."' "); // Kijkt in de database of er een gebruiker bestaat met het opgegeven naam + wachtwoord
-
-print($db->error);
-
-print_r($res);
 ?>
+
+<form action="" method="post">
+    <input type="text" name="login" value="test"/><br/>
+    <input type="password" name="password" value="test"/><br/>
+    <input type="submit" name="" value="Sturen"/><br/>
+</form>
