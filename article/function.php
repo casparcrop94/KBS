392990019126a4e4d973ebf2ac80134403f436a2
Richard van den Hoorn <hoornrichard@hotmail.com>
2012-11-19 11:54:30 +0100

    apart functions.php aangemaakt

---
+++ article/function.php
@@ -0,0 +1,39 @@
+<?php
+//Function to load the categorys from database
+function ophalencategorieen()
+{
+    $db = connectToDatabase();
+    $sth = $db->prepare ("SELECT * FROM category");
+    $sth->execute();
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
+//Get date of today
+function datumvandaag()
+{
+    $db = connectToDatabase();
+    $sth = $db->prepare ("SELECT NOW()");
+    $sth->execute();
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+       
+    foreach($result as $row)
+        {
+            $datum=$row["NOW()"]; 
+	}
+    return $datum;
+}
+//Get date of today
+function datumvandaag()
+{
+    $db = connectToDatabase();
+    $sth = $db->prepare ("SELECT NOW()");
+    $sth->execute();
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+       
+    foreach($result as $row)
+        {
+            $datum=$row["NOW()"]; 
+	}
+    return $datum;
+}
+?>
--- article/index.php
+++ article/index.php
@@ -7,49 +7,49 @@
 
 <body>
 <?php
+//Include files to connect with database
 include dirname(__FILE__) . '/../inc/config.inc.php';
 include DOCROOT . 'inc/mysql.inc.php';
+//include function.php
+include DOCROOT . 'articles/function.php';
+
+//Check if form is submitted
 if(isset($_POST['submit']))
 {
-    $title = $_POST['title'];
+    //Get data from form and set data into variable.
+	$title = $_POST['title'];
     $category = $_POST['category'];
     $date_added = $_POST['date_added'];
     $date_edited = $_POST['date_edited'];
     $text = $_POST['text'];
+	$published = '1';
     
-
+	//Insert data into database.
     mysql_query("INSERT INTO article (cat_id, date added, date_edited, titel, text, published) VALUES ('$category','$date_added','$date_edited','$title', '$text', '$published')");
+
 }
 
-function ophalencategorieen()
-{
-    $db = connectToDatabase();
-    $sth = $db->prepare ("SELECT * FROM category");
-    $sth->execute();
-    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-    return $result;
+//Get option and id
+$option=$_GET['option'];
+$id=$_GET['id'];
+
+//Check which option is to be used.
+if($option=='new'){
+	$date_added=datetoday();
+	$date_edited=datetoday();
+}
+elseif($option=='edit'){
+	$date_added=date_added($id);
+	$date_edited=datetoday();
 }
 
-function datumvandaag()
-{
-    $db = connectToDatabase();
-    $sth = $db->prepare ("SELECT NOW()");
-    $sth->execute();
-    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-       
-    foreach($result as $row)
-        {
-            $datum=$row["NOW()"]; 
-	}
-    return $datum;
-}
 
 ?>
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 	<input name="title" type="text" size="80" />
     <select name="category">
     	<?php 
-		$cat = ophalencategorieen();
+		$cat = getcategory();
 		foreach($cat as $row)
 		{
 			print("<option value=".$row["cat_id"].">"); 
@@ -57,11 +57,12 @@ function datumvandaag()
 		}
         ?>
 	</select>
-    <input name="date_added" type="text" value="<?php echo datumvandaag(); ?>" />
-    <input name="date_edited" type="text" value="<?php echo datumvandaag(); ?>" />
+    <input name="date_added" type="text" value="<?php echo $date_added; ?>" />
+    <input name="date_edited" type="text" value="<?php echo $date_edited; ?>" />
     <textarea name="text" rows="4" cols="20">
     </textarea>
-    <input type="submit" value="Opslaan" name="submit" />    
+    <input type="submit" value="Opslaan" name="submit" /> 
+	
 </form>
 
 
