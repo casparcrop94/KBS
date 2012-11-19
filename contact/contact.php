6f1924a1ebaee5e533441848dc62c710dcbc46e7
Maarten Engels <maartendeboy@hotmail.com>
2012-11-19 15:38:17 +0100

    contact-bestanden netter gemaakt
    Kleine deeltjes aangepast

--- contact/contact.php
+++ contact/contact.php
@@ -1,16 +1,19 @@
 <?php
+ini_set("SMTP", "test.smtp.org");
 $naam = $_POST['naam'];
 $email = $_POST['email'];
 $vraag = $_POST['vraag'];
 
-$to = 'maartendeboy@hotmail.com';
+//$to = 'maartendeboy@hotmail.com';
+$to = 'bouncer@test.smtp.org';
 $subject = 'Vraag van bezoeker '.$naam;
 
 $message = 'From: '.$naam."\n";
 $message .= 'E-mail: '.$email."\n";
 $message .= 'Message: '.$vraag;
 
-$headers = 'From: '.$email."\r\n";
+//$headers = 'From: '.$email."\r\n";
+$headers = 'From: maartendeboy@hotmail.com\r\n';
 $headers .= 'Reply-To: '.$email."\r\n";
 
 $mail_status = mail($to, $subject, $message, $headers);
@@ -24,7 +27,7 @@ if ($mail_status) { ?>
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to gordon@template-help.com');
+		alert('Bericht versturen mislukt. Probeer opnieuw of stuur een mail naar maartendeboy@hotmail.com');
 		window.location = 'contact.html';
 	</script>
 <?php
