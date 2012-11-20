df4a60716feeedb39028ce7a15bc39d502fdd984
Maarten Engels <maartendeboy@hotmail.com>
2012-11-20 15:59:05 +0100

    Maarten's config.inc.php zodat alles werkt met wamp

---
+++ inc/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DOCROOT', dirname(__FILE__) . '/../');
+define('SERVERPATH', 'http://kbs.nl');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_PORT', '');
+define('DB_DATABASE', 'kbs');
