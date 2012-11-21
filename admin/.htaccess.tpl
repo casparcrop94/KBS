5808e747a4b625784b1cc2105d01dba798f401e3
Robert-John van Doesburg <rjvandoesburg@hotmail.com>
2012-11-21 15:58:06 +0100

    Admin gedeelte toegevoegd
    Htaccess aangepast
    login pagina naar admin verplaatst

--- admin/.htaccess.tpl
+++ admin/.htaccess.tpl
@@ -8,5 +8,5 @@ RewriteBase /
 RewriteCond %{SCRIPT_FILENAME} !-f
 RewriteCond %{SCRIPT_FILENAME} !-d
 
-RewriteRule ([a-zA-Z]+) index.php?p=$1 [NC]
-#RewriteRule ([a-zA-Z]+)/([0-9]+)$ index.php?p=$1&id=$2 [NC]
+RewriteRule ^login$ /admin/login.php [NC]
+RewriteRule ^([a-zA-Z]+)$ /admin/index.php?p=$1 [NC]
--- admin/index.php
+++ admin/index.php
@@ -6,9 +6,14 @@ ob_start();
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
+if(!isset($_SESSION['admin']) && $_SESSION['admin'] != true)
+{
+	header('location:/admin/login');
+}
+
 $page = isset($_GET['p']) ? $_GET['p']:'home';
 
-if(!file_exists(DOCROOT . 'templates/' . $page . '.php'))
+if(!file_exists(DOCROOT . 'admin/templates/' . $page . '.php'))
 {
 	$page = 'home';
 }
@@ -16,23 +21,18 @@ if(!file_exists(DOCROOT . 'templates/' . $page . '.php'))
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>Juridische hulp</title>
-		<link rel="stylesheet" type="text/css" href="/styles/default.css" />
+		<title>Juridische hulp - Admin</title>
+		<link rel="stylesheet" type="text/css" href="/styles/admin.css" />
 		
 		<script type="text/javascript" src="/scripts/jquery.1.8.3.js"></script>
-		<script type="text/javascript" src="/scripts/default.js"></script>
+		<script type="text/javascript" src="/scripts/admin.js"></script>
 	</head>
 	<body>
 		<div id="wrapper">
-			<div id="header">
-				<div id="header-image"></div>
-				<div id="menu"></div>
-			</div>
 			<div id="content">
-				<div id="left-content">
-					<?php include '/templates/' . $page . '.php';?>
+				<div id="content">
+					<?php include DOCROOT . 'admin/templates/' . $page . '.php';?>
 				</div>
-				<div id="right-content"></div>
 			</div>
 			<div id="footer"></div>
 		</div>
