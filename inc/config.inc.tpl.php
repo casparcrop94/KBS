ab70122365705d43e4c1ff09eb85762d3dee1fa4
Robert-John van Doesburg <rjvandoesburg@gmail.com>
2012-11-26 13:35:48 +0100

    gitignore en config

---
+++ inc/config.inc.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DOCROOT', dirname(__FILE__) . '/../');
+define('SERVERPATH', 'http://kbs.nl');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'usbw');
+define('DB_PORT', '3307');
+define('DB_DATABASE', 'kbs');
