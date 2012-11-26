aa8328428d6cbe2b378697d978fe37391530a5ff
Erik de Vries <erik_vries@live.nl>
2012-11-26 15:13:43 +0100

    //aangepast

--- downloads/Klantinziendocumenten.php
+++ downloads/Klantinziendocumenten.php
@@ -21,7 +21,7 @@ $dbh= connectToDatabase();
         <td> <?php echo ($row["file"]); ?> </td>
         <?php // Laat de size van het bestand zien in kb.?>
         <td> <?php echo ($row["size"]); ?> kb </td>
-        <?php // Met deze functie kan je bestanden downloaden die geupload zijn.?>
+        <?php // Met deze functie kan je bestanden openen die geupload zijn.?>
         <td> <a href=http://kbs.nl/uploads/<?php echo ($row["file"]); ?> >Download</a> </td>
     </tr>    
         
