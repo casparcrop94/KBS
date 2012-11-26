1d70e80ee5e626ccc18f691cb63e55e6b521ac0a
Richard van den Hoorn <hoornrichard@hotmail.com>
2012-11-26 15:29:31 +0100

    map category toegevoegd

---
+++ category/index.php
@@ -0,0 +1,57 @@
+<?php
+include(DOCROOT."/inc/mysql.inc.php");
+
+$dbh = connectToDatabase();  // Maak verbinding met de database
+$statusText = "";
+
+if(isset($_GET['option'])) {
+    if($_GET['option'] == "delete") {
+        $sth = $dbh->prepare("DELETE FROM category WHERE ID=:id");
+        $sth->bindParam(":id", $_GET['id']);
+        $sth->execute();
+        
+        $statusText = "Categorie succesvol verwijderd.";
+    }
+}
+
+if(isset($_GET["case"])) { 
+    if($_GET["case"] == "succes") { 
+        $statusText = "Categorie succesvol opgeslagen.";
+    } else { 
+        $statusText = "Categorie niet succesvol opgeslagen.";
+    }
+}
+
+$sth = $dbh->query("SELECT * FROM category ORDER BY ID"); // Haal alle categorien uit de database
+$sth->execute();
+
+$res = $sth->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        echo($statusText."<br/>");
+        ?>
+        <table border="1">
+            <tr>
+                <td>Naam</td> 
+                <td>Beschrijving</td>
+				<td>Gepubliceerd</td>
+				<td>Bewerk</td>
+				<td>Verwijder</td>                
+            </tr>
+            <?php 
+                foreach($res as $row) {                                                // Loop door SQL-resultaten
+                    echo("<tr>");
+                    echo("<td>".$row['name']."</td>");                                 // Print de titel
+                    echo("<td>".$row['discription']."</td>");                          // Print de beschrijving
+                    echo("<td>".($row['published'] == 1 ? "Ja" : "Nee")."</td>");      // Print de publicatiestatus
+                    echo("<td><a href='category.php?option=edit&id=".$row['ID']."'>Bewerk</a></td>");      // Print de bewerk knop
+                    echo("<td><a href='".$_SERVER['PHP_SELF']."?option=delete&id=".$row['ID']."'>Verwijder</a></td>"); // Print de verwijder knop
+                    echo("</tr>");
+                } 
+            ?>
+        </table>
+    </body>
+</html>
