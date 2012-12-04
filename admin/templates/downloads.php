0f98bfd55c38c5eb33bde0df8d801c8bfab51530
Erik de Vries <erik_vries@live.nl>
2012-12-04 12:59:43 +0100

    downloadtest

--- admin/templates/downloads.php
+++ admin/templates/downloads.php
@@ -6,19 +6,19 @@ $dbh = connectToDatabase();
 if (isset($_GET['action'])) {
 
     if ($_GET['action'] == 'delete') {
-        $id = $_GET['ID'];
-        $sth = $dbh->prepare("SELECT file FROM downloads WHERE ID=:id");
-        $sth->bindParam(":id", $id, PDO::PARAM_STR);
-        $sth->execute();
-        $result = $sth->fetch(PDO::FETCH_ASSOC);
+	$id = $_GET['ID'];
+	$sth = $dbh->prepare("SELECT file FROM downloads WHERE ID=:id");
+	$sth->bindParam(":id", $id, PDO::PARAM_STR);
+	$sth->execute();
+	$result = $sth->fetch(PDO::FETCH_ASSOC);
 
-        if (unlink(DOCROOT . 'uploads/' . $result["file"])) {
-            $sth = $dbh->prepare("DELETE FROM downloads Where ID=:id");
-            $sth->bindParam(":id", $id, PDO::PARAM_STR);
-            $sth->execute();
-            header("location: /admin/downloads");
-            exit;
-        }
+	if (unlink(DOCROOT . 'uploads/' . $result["file"])) {
+	    $sth = $dbh->prepare("DELETE FROM downloads Where ID=:id");
+	    $sth->bindParam(":id", $id, PDO::PARAM_STR);
+	    $sth->execute();
+	    header("location: /admin/downloads");
+	    exit;
+	}
     }
 }
 if (isset($_POST['submit'])) {
@@ -30,28 +30,28 @@ if (isset($_POST['submit'])) {
     $extension = end($explode);
     // de size van hoe groot het bestand maximaal mag worden in kb.
     if ($_FILES["file"]["size"] < 8000000
-            && in_array($extension, $allowedExts)) {
-        if ($_FILES["file"]["error"] > 0) {
-            echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-        } else {
-            // echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-            // echo "Type: " . $_FILES["file"]["type"] . "<br />";
-            // echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-            // echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-            // upload
-            if (file_exists(DOCROOT . 'uploads/' . $_FILES["file"]["name"])) {
-                echo $_FILES["file"]["name"] . " bestaat al. ";
-            } else {
-                if (move_uploaded_file($_FILES["file"]["tmp_name"], DOCROOT . 'uploads/' . $_FILES["file"]["name"])) {
-                    //db
-                    $sth = $dbh->prepare("INSERT INTO downloads (file, size) 
+	    && in_array($extension, $allowedExts)) {
+	if ($_FILES["file"]["error"] > 0) {
+	    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+	} else {
+	    // echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+	    // echo "Type: " . $_FILES["file"]["type"] . "<br />";
+	    // echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+	    // echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+	    // upload
+	    if (file_exists(DOCROOT . 'uploads/' . $_FILES["file"]["name"])) {
+		echo $_FILES["file"]["name"] . " bestaat al. ";
+	    } else {
+		if (move_uploaded_file($_FILES["file"]["tmp_name"], DOCROOT . 'uploads/' . $_FILES["file"]["name"])) {
+		    //db
+		    $sth = $dbh->prepare("INSERT INTO downloads (file, size) 
                                  VALUES('$file' , '$size')");
-                    $sth->execute();
-                }
-            }
-        }
+		    $sth->execute();
+		}
+	    }
+	}
     } else {
-        echo "Invalid file";
+	echo "Invalid file";
     }
 }
 
@@ -68,14 +68,14 @@ $result = $sth->fetchAll(PDO::FETCH_ASSOC);
         <th> Verwijder </th>
     </tr>
     <?php foreach ($result as $row) {
-        ?>
+	?>
         <tr>
-            <!-- Laat het bestand naam zien. -->
-            <td> <?php echo ($row["file"]); ?> </td>
-            <!-- Laat de size van het bestand zien in kb. -->
-            <td> <?php echo ($row["size"]); ?> kb </td>
-            <!-- Verwijder functie, verwijdert uit de map en de database. -->
-            <td> <a href="/admin/downloads/delete/<?php echo $row["ID"]; ?>">Verwijder</a></td>
+    	<!-- Laat het bestand naam zien. -->
+    	<td> <?php echo ($row["file"]); ?> </td>
+    	<!-- Laat de size van het bestand zien in kb. -->
+    	<td> <?php echo ($row["size"]); ?> kb </td>
+    	<!-- Verwijder functie, verwijdert uit de map en de database. -->
+    	<td> <a href="/admin/downloads/delete/<?php echo $row["ID"]; ?>">Verwijder</a></td>
         </tr>    
 
     <?php } ?>       
