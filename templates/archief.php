f01aa5d7927b3da0d5a715869cbb079edc99da09
Caspar <casparcrop@msn.com>
2012-12-09 00:30:19 +0100

    archief ietsjes bijgewerkt

--- templates/archief.php
+++ templates/archief.php
@@ -5,8 +5,13 @@
 ?>
 
 <div id="Archives">
-    <div id="Archivetitle"><h1>Archief</h1></div>
+    
+    <div id="Archivetitle">
+	<h1>Archief</h1>
+    </div>
+    
     <div id="Archive">
 	
     </div>
+    
 </div>
