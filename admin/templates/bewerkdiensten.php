74cc85cd5c811319a87068e6bbc8c126eda7d591
Richard van den Hoorn <hoornrichard@hotmail.com>
2012-12-10 15:49:23 +0100

    bewerkdiensten aangepast

--- admin/templates/bewerkdiensten.php
+++ admin/templates/bewerkdiensten.php
@@ -9,42 +9,45 @@ if(isset($_POST['submit']))
 {
 	//Get data from form and set data into variable.
     $option = $_POST['option'];
-	$id = $_POST['id'];
-	$title = $_POST['title'];	
-    $date_added = $_POST['date_added'];
-    $date_edited = $_POST['date_edited'];
-    $text = $_POST['text'];
+	$service_id = $_POST['service_id'];
+	$servicename = $_POST['service_name'];	
+	$servicetext = $_POST['servicetext'];
+    $pph = $_POST['pph'];
+    $avgcost = $_POST['avgcost'];
+    $image = $_POST['image'];
+    $article_id = $_POST['article_id'];
 	$published = $_POST['published'];
-	
-	//Format date to databaseformat
-	$date_added = date('Y-m-d H:i:s', strtotime($date_added));
-    $date_edited = date('Y-m-d H:i:s', strtotime($date_edited));
 	
 	if($option=='new')
 	{
-		//Insert new article
-		$db = connectToDatabase();
-		$sth = $db->prepare ("INSERT INTO services (cat_id, date_added, date_edited, title, text, published) 
-		VALUES (:category,:date_added,:date_edited,:title, :text,:published)");
-		$sth->bindParam(":title", $title);
-		$sth->bindParam(":text", $text);
+		//Insert new service
+		$sth = $db->prepare ("INSERT INTO services (servicename, servicetext, pph, avgcost, image, article_id,published) 
+		VALUES (:servicename, :servicetext, :pph, :avgcost, :image, :article_id, :published)");
+		$sth->bindParam(":servicename", $servicename);
+		$sth->bindParam(":servicetext", $servicetext);
+		$sth->bindParam(":pph", $pph);
+		$sth->bindParam(":avgcost", $avgcost);
+		$sth->bindParam(":image", $image);
+		$sth->bindParam(":article_id", $article_id);
 		$sth->bindParam(":published", $published);
 		$result=$sth->execute();
 	}
 	elseif($option=='edit')
 	{
-		//Edit an article
-		$db = connectToDatabase();
-		$sth = $db->prepare ("UPDATE services SET cat_id=:category,date_added=:date_added,date_edited=:date_edited,
-		title=:title,text=:text,published=:published WHERE ID=:id");
-		$sth->bindParam(":title", $title);
-		$sth->bindParam(":text", $text);
+		//Edit a service
+		$sth = $db->prepare ("UPDATE services SET service_id=:service_id, servicetext=:servicetext, pph=:pph, avgcost=:avgcost, image=:image, article_id=:article_id, published=:published WHERE service_id=:service_id");
+		$sth->bindParam(":service_id", $service_id);
+		$sth->bindParam(":servicename", $servicename);
+		$sth->bindParam(":servicetext", $servicetext);
+		$sth->bindParam(":pph", $pph);
+		$sth->bindParam(":avgcost", $avgcost);
+		$sth->bindParam(":image", $image);
+		$sth->bindParam(":article_id", $article_id);
 		$sth->bindParam(":published", $published);
-		$sth->bindParam(":id", $id);
 		$result=$sth->execute();
 	}
 	
-	//If article is saved succesfully, user get a message
+	//If service is saved succesfully, user get a message
 	if($result==1){
 		$case="succes";
 	}
@@ -53,7 +56,7 @@ if(isset($_POST['submit']))
 	}
 	
 	//After saving redirect to overview page
-	header("Location: /admin/artikel/".$case);
+	header("Location: /admin/diensten/".$case);
 	exit;
 }
 
@@ -62,28 +65,32 @@ $option= isset($_GET["option"])?$_GET['option']:'new';
 
 //Check which option is to be used.
 if($option=='new'){
-	$id='';
-	$date_added=date('d-m-Y H:i:s');
-	$date_edited=date('d-m-Y H:i:s');
-	$title =''; 
-	$text = '';
+	$title='';
+	$servicetext='';
+	$pph='';
+	$avgcost='';
+	$image='';
 	$published =" selected";
 	$unpublished ='';
 }
 elseif($option=='edit'){
 	//Get the ID from the URL
-	$id= isset($_GET["id"])?$_GET['id']:'';
+	$service_id= isset($_GET["id"])?$_GET['id']:'';
 	
 	//Get content from database where ID=$id
-	$sth = $db->prepare ("SELECT * FROM services WHERE service_id=$id");
+	$sth = $db->prepare ("SELECT * FROM services WHERE service_id=$service_id");
     $sth->execute();
     $content = $sth->fetchAll(PDO::FETCH_ASSOC);
 	
 	//Set the data into variables
 	foreach($content as $row)
 	{
-		$title =$row["title"]; 
-		$text = $row["text"];
+		$title =$row["servicename"]; 
+		$pph = $row["pph"];
+		$avgcost = $row["avgcost"];
+		$image = $row["image"];
+		$article_id = $row["article_id"];
+		$servicetext = $row["servicetext"];		
 		$published = $row["published"];
 	}
 	//Check if article is published
@@ -97,12 +104,23 @@ elseif($option=='edit'){
 		$published='';
 		$unpublished=" selected";
 	}
+	
+	
+}
+
+function haalartikelenop(){
+	$db = connectToDatabase();
+	$sth = $db->prepare ("SELECT * FROM article WHERE cat_id=(SELECT cat_id FROM category WHERE name='Diensten')");
+	$sth->execute();
+	$articles = $sth->fetchAll(PDO::FETCH_ASSOC);
+	
+	return $articles;
 }
 ?>
 
 <form action="" method="post">
 <input name="option" type="hidden" value="<?php echo $option; ?>" />
-<input name="id" type="hidden" value="<?php echo $id; ?>" />	
+<input name="service_id" type="hidden" value="<?php echo $service_id; ?>" />	
 <table>
 	<tr>
     	<td colspan="2">
@@ -111,16 +129,64 @@ elseif($option=='edit'){
     </tr>
     <tr>
         <td colspan="2">
-        	<input name="title" type="text" size="80" value="<?php echo $title; ?>"/>
+        	<input name="service_name" type="text" size="80" value="<?php echo $title; ?>"/>
         </td>
     </tr>
-    <td colspan="2">
+    <tr>
+    	<td>
+        	Prijs per uur:
+        </td>
+        <td>
+        	<input name="pph" type="text" value="<?php echo $pph; ?>"/>
+        </td>
+    </tr>
+    <tr>
+    	<td>
+        	Gemiddelde prijs:
+        </td>
+        <td>
+        	<input name="avgcost" type="text" value="<?php echo $avgcost; ?>"/>
+        </td>
+    </tr>
+    <tr>
+    	<td>
+        	Afbeelding:
+        </td>
+        <td>
+        	<!--  HIER MOET UPLOAD FUNCTIE KOMEN, SCHIET EENS OP ERIK -->
+        	<input name="image" type="text" value="<?php echo $image; ?>"/>
+        </td>
+    </tr>
+    <tr>
+    	<td>
+        	Gelinkt artikel
+        </td>
+        <td>
+        	<select name="article_id">
+        	<?php 
+        	$articles= haalartikelenop();
+        	foreach($articles as $row){
+        		
+        		if($row['ID']==$article_id){
+        			$selected='selected';
+          		}
+          		else{
+          			$selected='';
+          		}
+        		print("<option value='".$row['ID']."'".$selected.">".$row['title']."</option>");
+        	}
+        	?>
+   			 </select>
+        </td>
+    </tr>
+    <tr>
+    	<td colspan="2">
         	Text:
        	</td>
     </tr>
     <tr>
         <td colspan="2">
-        	<textarea name="text" rows="20" cols="70"><?php echo $text; ?></textarea>
+        	<textarea name="servicetext" rows="20" cols="70"><?php echo $servicetext; ?></textarea>
         </td>
     <tr />
     <tr>
