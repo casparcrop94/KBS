de63ae810cd1ae23b31d5b001749599c4e4e65bd
Caspar <casparcrop@msn.com>
2012-12-10 13:47:28 +0100

    fouten opgelost in mergen

--- templates/contact.php
+++ templates/contact.php
@@ -1,66 +1,8 @@
-<<<<<<< HEAD
 <!--
 Auteur: Maarten Engels
 -->
 <?php
 //Het zelfafhandelende gedeelte, waarbij het formulier wordt getoond als nog niet op de verstuur-knop is gedrukt
-if (isset($_POST["verstuur"])) { 
-    $naam = $_POST['naam'];
-    $email = $_POST['email'];
-    $onderwerp = $_POST['onderwerp'];
-    $vraag = $_POST['vraag'];
-    
-    //Eigenaar's email, dus gerelateerde van Caspar (nu voor testen nog mijn e-mail
-    $to = EMAIL_KLANT;   
-    $subject = 'Vraag van bezoeker ' . $naam;
-
-    //Hier de info over de mail en de vraag
-    $message = 'Van: ' . $naam . "\n";
-    $message .= 'E-mail: ' . $email . "\n";
-    $message .= 'Onderwerp: ' . $onderwerp;
-    $message .= 'Vraag: <br>' . $vraag;
-
-    //$headers = 'From: '.$email."\r\n";
-    $headers = 'From: ' . EMAIL_KLANT . '\r\n';
-    $headers .= 'Reply-To: ' . $email . "\r\n";
-
-    $mail_status = mail($to, $subject, $message, $headers);
-    
-    if (!$mail_status) {
-        print("Helaas, het versturen van de mail is mislukt");
-    } else {
-        print("Geslaagd, de mail is verstuurd");
-    }
-}
-?>
-
-<form method="post">
-    <table>
-        <tr>
-            <td>Naam</td>
-            <td><input type="text" name="naam"></td>
-        </tr>
-        <tr>
-            <td>E-mailadres</td>
-            <td><input type="text" name="email"></td>
-        </tr>
-        <tr>
-            <td>Onderwerp</td>
-            <td><input type="text" name="onderwerp"></td>
-        </tr>
-        <tr>
-            <td>Vraag</td>
-        </tr>
-        <tr>
-            <td colspan="2"><textarea name="vraag"></textarea></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="verstuur" value="Versturen"></td>
-        </tr>
-    </table>
-</form>
-=======
-<?php
 if (isset($_POST["verstuur"])){
     $naam = $_POST['naam'];
     $email = $_POST['email'];
@@ -117,4 +59,3 @@ if (isset($_POST["verstuur"])){
                 </tr>
             </table>
         </form>
->>>>>>> 32ad56b634ec67bea4b6bb3b5c884691bc75a258
--- templates/downloads.php
+++ templates/downloads.php
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<?php
-include DOCROOT . 'inc/mysql.inc.php';
-// db
-$dbh= connectToDatabase();
-    $sth = $dbh->prepare ("SELECT * FROM downloads");
-    $sth->execute();
-    $result = $sth->fetchAll(PDO::FETCH_ASSOC);   
-?>  
-
-<table border="1">
-    <tr>    
-        <th> Bestanden </th>    
-        <th> Grootte </th>
-        <th> Download </th>
-    </tr>
-       <?php foreach($result as $row) {
-?>
-    <tr>
-        <!-- Laat de bestand naam zien.-->
-        <td> <?php echo ($row["file"]); ?> </td>
-        <!-- Laat de size van het bestand zien in kb.-->
-        <td> <?php echo ($row["size"]); ?> kb </td>
-        <!--  Met deze functie kan je bestanden downloaden die geupload zijn. -->
-        <td> <a href=http://kbs.nl/uploads/<?php echo rawurlencode($row["file"]) ?> >Download</a> </td>
-    </tr>    
-        
- <?php } ?>       
-     </table>
-   
-=======
-<?php
-include DOCROOT . 'inc/mysql.inc.php';
-// db
-$dbh= connectToDatabase();
-    $sth = $dbh->prepare ("SELECT * FROM downloads");
-    $sth->execute();
-    $result = $sth->fetchAll(PDO::FETCH_ASSOC);   
-?>  
-
-<table border="1">
-    <tr>    
-        <th> Bestanden </th>    
-        <th> Grootte </th>
-        <th> Download </th>
-    </tr>
-       <?php foreach($result as $row) {
-?>
-    <tr>
-        <!-- Laat het bestand naam zien. -->
-        <td> <?php echo ($row["file"]); ?> </td>
-        <!-- Laat de size van het bestand zien in kb. -->
-        <td> <?php echo ($row["size"]); ?> kb </td>
-        <!-- Met deze functie kan je bestanden downloaden die geupload zijn. -->
-        <td> <a href=http://kbs.nl/uploads/<?php echo rawurlencode($row["file"]) ?> >Download</a> </td>
-    </tr>    
-        
- <?php } ?>       
-     </table>
-   
->>>>>>> 17151fa8e30ae588b3ed17839af09b4f04faa0d7
-    
-=======
 <!-- Erik de Vries -->
 <?php
 include DOCROOT . 'inc/mysql.inc.php';
@@ -91,5 +27,3 @@ $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
     <?php } ?>       
 </table>
-
->>>>>>> 32ad56b634ec67bea4b6bb3b5c884691bc75a258
