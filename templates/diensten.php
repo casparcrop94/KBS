c66f6363ec499778de7c5a0f22bbaf33fe8bd43d
Richard van den Hoorn <hoornrichard@hotmail.com>
2012-12-10 13:00:46 +0100

    admin diensten gemaakt

--- templates/diensten.php
+++ templates/diensten.php
@@ -4,22 +4,22 @@ include('/inc/mysql.inc.php');
 
 $dbh = connectToDatabase();
 
-$sth = $dbh->query("SELECT * FROM services"); // Haal alle artikelen uit de database
+$sth = $dbh->query("SELECT * FROM services"); // Haal alle diensten uit de database
 $sth->execute();
 $res = $sth->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-        <table width="700">
+        <table width="auto">
             <?php 
             	$i=0;
                 foreach($res as $row) {  // Loop door SQL-resultaten
                 	if($i==0){
                 		echo("<tr>");
                 	}
-                    echo("<td width=50% onclick=window.location='".$row['article_id']."'; style='cursor: pointer';>");
-                    echo("<div class=title>".$row['servicename']."</div>");
+                    echo("<td class=diensten onclick=window.location='".$row['article_id']."'; style='cursor: pointer';>");
+                    echo("<div class=title><h2>".$row['servicename']."</h2></div>");
+                    echo("<br /><div class=image><img src=".$row['image']."heigt=60 /></div>");
                     echo("<br /><div class=discription>".$row['servicetext']."</div>");
-                    echo("<br /><div class=image><img src=".$row['image']."/></div>");
                     echo("</td>");
                     if($i==1){
                     	echo("</tr>");
