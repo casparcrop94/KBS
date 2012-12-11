9640fd5b75d014d411c1718223749c9185c1b630
Jelle Kapitein <gitschool@kapitei.nl>
2012-12-11 11:51:51 +0100

    Frontpage artikelen
    niet klaar nooit klaar never klaar

--- scripts/default.js
+++ scripts/default.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-	
+
+$("#fold-year").live("click", function(event){
+	var id = $(this).attr("rel");
+	var html = $(this).html();
+	var icon = (html == "? "? '? ':'? ');
+	$(this).html(icon)
+
+	$("#" + id).toggle('slow', function() {
+	    // Animation complete.
+	  });
+	event.preventDefault();
+});
+
+$("#fold-month").live("click", function(event){
+	var id = $(this).attr("rel");
+	$("#" + id).toggle('slow', function() {
+	    // Animation complete.
+	  });
+	event.preventDefault();
+});
+
 });
