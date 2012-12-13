86b8ab2fd37179b534b93ec826c87839a9b4571b
Caspar <casparcrop@msn.com>
2012-12-13 12:02:27 +0100

    home uitgebreid

--- templates/home.php
+++ templates/home.php
@@ -1,1 +1,17 @@
-Home
+<div>
+    <div id="home-upper">
+	<div id="home-about">
+	    Dit stukje is voor uitleg van het bedrijf
+	</div>
+	<div id="home-links">
+	    Hier staan links naar andere sites / social media
+	</div>
+    </div>
+    
+    <div id="home-news">
+	Hier komen de artikelen
+    </div>
+    
+    
+    
+</div>
