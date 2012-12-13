c7686d4c66585f18de3dce5fe156c015cb8c3181
Caspar <casparcrop@msn.com>
2012-12-13 14:02:24 +0100

    uitbreiding homepagina

--- templates/home.php
+++ templates/home.php
@@ -5,10 +5,20 @@ $dbh=  connectToDatabase();
 <div>
     <div id="home-upper">
 	<div id="home-about">
-	    Dit stukje is voor uitleg van het bedrijf
+	    Duis neque nisi, dapibus sed mattis quis, rutrum et accumsan. 
+	    Suspendisse nibh. Suspendisse vitae magna eget odio amet mollis 
+	    justo facilisis quis. Sed sagittis mauris amet tellus gravida 
+	    lorem ipsum dolor sit amet consequat blandit lorem ipsum dolor 
+	    sit amet consequat sed dolore
 	</div>
 	<div id="home-links">
-	    Hier staan links naar andere sites / social media
+	    <ul>
+		<li>facebook</li>
+		<li>twitter</li>
+		<li>linkedin</li>
+		<li>nogiets</li>
+		<li>nog eens nog iets</li>
+	    </ul>
 	</div>
     </div>
     
@@ -17,8 +27,8 @@ $dbh=  connectToDatabase();
 	$result=  retreivenewsarticle($dbh);
 	foreach($result as $row){
 	    echo'<div id="home-article">';
-	    echo($row['title']);
-	    echo($row['TEXT']);
+	    echo('<h3>'.$row["title"].'</h3>');
+	    echo($row["TEXT"]);
 	    echo'</div>';
 	}
 	?>
