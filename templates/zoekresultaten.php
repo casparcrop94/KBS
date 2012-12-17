3454efbd66d0d5bec1141be9c71481fe3530677c
Maarten Engels <maartendeboy@hotmail.com>
2012-12-17 17:33:29 +0100

    d

--- templates/zoekresultaten.php
+++ templates/zoekresultaten.php
@@ -3,10 +3,10 @@ Auteur: Maarten Engels
 	s1058387
 	ICTM1e
 -->
-
+session_start();
 <?php
 //Het woord dat ingevuld wordt in de zoekbalk wordt opgehaald en gebruikt als attribuut voor de SQL-querie.
-$zoekwoord = $_POST["zoekwoord123"];
+$zoekwoord = mysql_real_escape_string($_POST["zoekwoord123"]);
 $_SESSION["zoekresultaat"] = $zoekwoord;
 //Zelfafhandelend formulier waarbij er naar resultatenpagina 1 gaat als er nog geen paginanummer is opgegeven
 if (isset($_GET["page"])) {
