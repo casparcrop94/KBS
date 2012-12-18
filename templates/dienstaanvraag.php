59c4552d9c99e19f643f5ca9ea8c2f022938b6a6
Caspar <casparcrop@msn.com>
2012-12-18 11:59:43 +0100

    dienstaanvraag wat verbeterd

--- templates/dienstaanvraag.php
+++ templates/dienstaanvraag.php
@@ -1,33 +1,75 @@
-<?php session_start(); ?>
 <!--Auteur: Caspar Crop-->
 <?php
-$dbh=  connectToDatabase();
+$dbh = connectToDatabase();
 
-
-if(isset($_POST['vraagaan'])){
+//if the user submitted the form, the following is done:
+if (isset($_POST['vraagaan'])) {
+//put all inputs in variables    
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $address=$_POST['address'];
+    $zipcode=$_POST['zipcode'];
+    $residence=$_POST['residence'];
+    $telephone=$_POST['telephone'];
+    $mobile=$_POST['mobile'];
+    $service=;
     
-    if($_POST['name']==""){
-	$namemiss=true;
+    if($name!="" or $email!="" or $address!="" or $zipcode!="" or $residence!="" or $telephone!="" or $mobile!=""){
+    //check agenda
+    
+	
+	
+	
+	
     }
-    else{$name=$_POST['name'];}
-    
-    if($_POST['email']){
-	$emailmiss=true;
+    else{
+	echo '<p>Niet alle gegevens zijn ingevuld!</p><br/><br/>';
+	?>
+	<form action="" method="POST">
+    <table>
+	<tr>
+	    <td>Naam:</td>
+	    <td><input type="text" name="name" value="<?php echo($name)?>" /></td>
+	</tr>
+	<tr>
+	    <td>E-mail</td>
+	    <td><input type="text" name="email" value="<?php echo($email)?>" /></td>
+	</tr>
+	<tr>
+	    <td>Adres</td>
+	    <td><input type="text" name="address" value="<?php echo($address)?>" /></td>
+	</tr>
+	<tr>
+	    <td>Postcode</td>
+	    <td><input type="text" name="zipcode" value="<?php echo($zipcode)?>" /></td>
+	</tr>
+	<tr>
+	    <td>Woonplaats</td>
+	    <td><input type="text" name="residence" value="<?php echo($residence)?>" /></td>
+	</tr>
+	<tr>
+	    <td>Telefoonnummer</td>
+	    <td><input type="text" name="telephone" value="<?php echo($telephone)?>" /></td>
+	</tr>
+	<tr>
+	    <td>Mobiel</td>
+	    <td><input type="text" name="mobile" value="<?php echo($mobile)?>" /></td>
+	</tr>
+	<tr>
+	<td><input type="hidden" name="service" value=""></td>
+	<td></td>
+	</tr>
+	<tr>
+	    <td></td>
+	    <td><input type="submit" name="vraagaan" value="Vraag aan!" /></td>
+	</tr>
+    </table>
+</form>
+    <?php
     }
-    else{$email=$_POST['email'];}
-    
-    
-    
-    
-    
 }
+else{
 ?>
-
-
-
-
-
-
 <form action="" method="POST">
     <table>
 	<tr>
@@ -59,11 +101,13 @@ if(isset($_POST['vraagaan'])){
 	    <td><input type="text" name="mobile" value="" /></td>
 	</tr>
 	<tr>
+	<td><input type="hidden" name="service" value=""></td>
+	<td></td>
+	</tr>
+	<tr>
 	    <td></td>
 	    <td><input type="submit" name="vraagaan" value="Vraag aan!" /></td>
 	</tr>
     </table>
 </form>
-
-
-
+<?php } ?>
