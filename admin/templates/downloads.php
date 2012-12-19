a051f364daa54fb1d439bf412a497ea66fd4d3bf
Erik de Vries <erik_vries@live.nl>
2012-12-19 18:05:37 +0100

    niks

--- admin/templates/downloads.php
+++ admin/templates/downloads.php
@@ -79,30 +79,30 @@ $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 	<table class="hover">
 	    <tr id="head"> 
 		<th class="center"><input type="checkbox" id="checkall" value=""/></th>
-	     <div class="page">
-	    <div class="pagination">   
-		<?php
+	    <div class="page">
+		<div class="pagination">   
+		    <?php
 //db
-		$sql2 = "SELECT * FROM downloads";
-		$result2 = selectquery($sql2, $dbh);
+		    $sql2 = "SELECT * FROM downloads";
+		    $result2 = selectquery($sql2, $dbh);
 //het aantal records, aantal word berekent door $result bij elkaar optetellen
-		$total_records = count($result2);
+		    $total_records = count($result2);
 //het aantal pages, aantal pages wordt berekend door het aantal records delen door 10
-		$total_pages = ceil($total_records / 20);
+		    $total_pages = ceil($total_records / 20);
 //$1 als er niet genoeg bestanden zijn voor 2 pagina's, is de pagination niet zichtbaar
-		if ($total_pages > 1) {
+		    if ($total_pages > 1) {
 //$1 staat voor de pagina nummer, begint op 1
-		    for ($i = 1; $i <= $total_pages; $i++) {
+			for ($i = 1; $i <= $total_pages; $i++) {
 // zorgt ervoor dat de paginanummer van de pagina waar hij nu op zit niet wordt weergegeven
-			if ($i != $page) {
+			    if ($i != $page) {
 //$1 (de pagina nummer) komt achter de url de staan en wordt weergegeven als $1 boven de tabel
-			    echo "<a href='/admin/downloads/" . $i . "'>" . $i . "</a> ";
+				echo "<a href='/admin/downloads/" . $i . "'>" . $i . "</a> ";
+			    }
 			}
 		    }
-		}
-		?>
-	    </div>
-	   
+		    ?>
+		</div>
+
 		<!-- geeft weer op welke pagina je zit -->
 		<p> <strong> Pagina: <?php echo $i = $page ?> </strong> </p>
 	    </div>
@@ -121,7 +121,6 @@ $result = $sth->fetchAll(PDO::FETCH_ASSOC);
     		<td> <?php echo ($row["size"]); ?> kb </td>
 
     	    </tr>    
-
 	    <?php } ?>       
 	</table>
     </form>
--- templates/downloads.php
+++ templates/downloads.php
@@ -17,32 +17,32 @@ $dbh = connectToDatabase();
 $sql1 = "SELECT * FROM downloads ORDER BY ID DESC LIMIT $start_from, 10";
 $result1 = selectquery($sql1, $dbh)
 ?>
- <div class="page">
-<div class="pagination">
-    <?php
+<div class="page">
+    <div class="pagination">
+	<?php
 //db
-    $sql2 = "SELECT * FROM downloads";
-    $result2 = selectquery($sql2, $dbh);
+	$sql2 = "SELECT * FROM downloads";
+	$result2 = selectquery($sql2, $dbh);
 //het aantal records, aantal word berekent door $result bij elkaar optetellen
-    $total_records = count($result2);
+	$total_records = count($result2);
 //het aantal pages, aantal pages wordt berekend door het aantal records delen door 10
-    $total_pages = ceil($total_records / 10);
+	$total_pages = ceil($total_records / 10);
 //$1 als er niet genoeg bestanden zijn voor 2 pagina's, is de pagination niet zichtbaar
-    if ($total_pages > 1) {
+	if ($total_pages > 1) {
 //$1 staat voor de pagina nummer, begint op 1
-	for ($i = 1; $i <= $total_pages; $i++) {
-	    // zorgt ervoor dat de paginanummer van de pagina waar hij nu op zit niet wordt weergegeven
-	    if ($i != $page) {
+	    for ($i = 1; $i <= $total_pages; $i++) {
+// zorgt ervoor dat de paginanummer van de pagina waar hij nu op zit niet wordt weergegeven
+		if ($i != $page) {
 //$1 (de pagina nummer) komt achter de url de staan en wordt weergegeven als $1 onder de tabel
-		echo "<a href='/downloads/" . $i . "'>" . $i . "</a> ";
+		    echo "<a href='/downloads/" . $i . "'>" . $i . "</a> ";
+		}
 	    }
 	}
-    }
-    ?>
+	?>
+    </div>
+    <!-- geeft weer op welke pagina je zit -->
+    <p> <strong> Pagina: <?php echo $i = $page ?> </strong> </p>
 </div>
-<!-- geeft weer op welke pagina je zit -->
-		<p> <strong> Pagina: <?php echo $i = $page ?> </strong> </p>
-	    </div>
 <div  id="downloads">
     <table>
 	<tr id="head">    
